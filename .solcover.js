4bc758a7a2d49ea7d9fef90ec3dc0b49f70561b6
Alejo Salles <alejosalles@gmail.com>
2018-04-10 18:38:52 -0300

    added babel deps to coveralls copied packages

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,4 @@
 module.exports = {
-  copyPackages: ['zeppelin-solidity', 'zos-core'],
+  copyPackages: ['zeppelin-solidity', 'zos-core', 'babel', 'babel-polyfill', 'babel-preset-es2015', 'babel-preset-stage-2', 'babel-preset-stage-3', 'babel-register'],
   skipFiles: ['Migrations.sol']
 };
