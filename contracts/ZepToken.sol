c6c5ca855d81744e9823b3f52351e8359087374e
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2018-04-13 13:37:28 -0300

    Add ZepToken documentation

--- contracts/ZepToken.sol
+++ contracts/ZepToken.sol
@@ -6,13 +6,29 @@ import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
 import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
 
+/**
+ * @title ZepToken
+ * @dev ZEP token contract including mintable, burnable and pausable functionalities
+ */
 contract ZepToken is Initializable, StandardToken, MintableToken, PausableToken, BurnableToken {
+  // Token name
   string public constant name = "Zep Token";
+
+  // Token symbol
   string public constant symbol = "ZEP";
+
+  // Token decimals
   uint8 public constant decimals = 18;
 
+  /**
+   * @dev Constructor function
+   */
   function ZepToken() public {}
 
+  /**
+   * @dev Initialization function, sets the owner
+   * @param _owner representing the address of the owner
+   */
   function initialize(address _owner) public isInitializer {
     owner = _owner;
   }
