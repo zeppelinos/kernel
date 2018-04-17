ffbdffe283db90046dac13f871d2dae1a34b46e5
Santiago Palladino <spalladino@gmail.com>
2018-04-17 16:57:12 -0300

    Update sample deployment scripts

--- scripts/initProject.js
+++
@@ -1,8 +0,0 @@
-// Create a project controller for your project on top of zOS
-// Run as: `npx truffle exec scripts/init_project.js --network NETWORK`
-
-global.artifacts = artifacts;
-global.web3 = web3;
-global.log = true;
-
-module.exports = require('../deploy/scripts/initProject');
