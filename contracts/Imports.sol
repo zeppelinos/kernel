1e5c626e6a765bf3e3750b1d377f6d84ee916dca
Alejo Salles <alejosalles@gmail.com>
2018-04-17 17:07:53 -0300

    including minor changes from zos-core renaming

--- contracts/Imports.sol
+++ contracts/Imports.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.4.21;
 
 import "zos-core/contracts/application/versioning/Package.sol";
 import "zos-core/contracts/application/versioning/ContractDirectory.sol";
-import "zos-core/contracts/application/versioning/AppContractDirectory.sol";
-import "zos-core/contracts/application/management/AppManager.sol";
+import "zos-core/contracts/application/management/AppDirectory.sol";
+import "zos-core/contracts/application/management/PackagedAppManager.sol";
 import "zos-core/contracts/application/management/UnversionedAppManager.sol";
